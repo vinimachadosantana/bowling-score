710999746de2be3a33890ee5888d372c428399a0
Vini Machado <viniksantana5@gmail.com>
2020-11-08 19:00:38 -0300

    Created parser unite similars method.

--- lib/text_parser.rb
+++ lib/text_parser.rb
@@ -14,7 +14,28 @@ class TextParser
   private
 
   def parser
-    JSON.parse(@txt.to_json).split("\n")
+    unite_similars(JSON.parse(@txt.to_json).split("\n"))
+  end
+
+  def unite_similars(parsed_txt)
+    player_array = []
+
+    parsed_txt.each_with_index do |item, index|
+      player_array << item.split(' ') if index == 0
+      player_array << item.split(' ') if index == 1
+
+      if player_array[0][0] == item.split(' ').first
+        player_array[0] << item.split(' ').last unless index == 0
+      end
+
+      if index > 0
+        if player_array[1][0] == item.split(' ').first
+          player_array[1] << item.split(' ').last unless index == 1
+        end
+      end
+    end
+
+    player_array
   end
 end
 
