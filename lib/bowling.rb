ed60abd0ce15cca4fb6451753ae406fad9307cbc
Vini Machado <viniksantana5@gmail.com>
2020-11-09 20:08:19 -0300

    Created useful printer methods.

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -15,5 +15,37 @@ class Bowling
     parsed_txt = @parse_txt.call
     pinfall = @pinfall.call(parsed_txt)
     scoring = @scoring.call(pinfall)
+
+    bowling_printer(pinfall, scoring)
+  end
+
+  private
+
+  def bowling_printer(pinfall, scoring)
+    frame_printer && results_printer(pinfall, scoring)
+  end
+
+  def frame_printer
+    str = 'Frame  '
+    range = (1..10).map { |frame| "#{frame}         " }.join
+
+    p "#{str}     #{range}".rstrip
+  end
+
+  def results_printer(pinfall, scoring)
+    pinfall.each do |pinfalls|
+      player_printer(pinfalls.shift) && pinfall_printer(pinfalls)
+    end
+  end
+
+  def player_printer(player)
+    p player
+  end
+
+  def pinfall_printer(pinfalls)
+    str = 'Pinfalls'
+    pinfall = pinfalls.flatten.map { |frame| "#{frame}    " }.join
+
+    p "#{str}    #{pinfall}".rstrip
   end
 end
